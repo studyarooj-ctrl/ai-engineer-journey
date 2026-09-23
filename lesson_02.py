7ace6a852409045dcd6b3c3513d0df2e47173154
studyarooj <266190612+studyarooj-ctrl@users.noreply.github.com>
2026-09-23 08:17:48 +0500

    Complete Lesson 1.2: variables and data types

---
+++ lesson_02.py
@@ -0,0 +1,29 @@
+# lets start lesson 2
+# where we learn specific strings int ,str,float,bool
+# we learn about type function or comments
+# we learn variables
+
+
+print('variables woh hotay hein jinme koi na koi data store ho. example ke taur pr ')
+name='arooj zahid'
+print(name)
+
+print('now we learn about data types. str are written in quotations, int are numbers, float are decimal numbers or booleans are decisions makes data types like True or False. or its case sensitive mean true or false are not valid')
+name='sarah'
+age=34
+weight=3.17
+are_they_fit=True
+print(name)
+print(age)
+print(weight)
+print(are_they_fit)
+has_finished_lesson = False
+print(has_finished_lesson)
+print(type(has_finished_lesson))
+
+print('now we learn about type function to check the datatype of variable')
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(are_they_fit))
+print('comments are written in # esky baad line comment mei shift ho jati hei or interpreter usko ignore kr deita hei')
